1055707617e0897523fb18cfc244c9ed7d38faf5
Eric <eric@edenx.io>
2018-06-24 15:28:27 +0200

    Playing with more paths

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ GLOBALPATH=$(dirname $(realpath $0))
 /bin/echo "Preparing and clearing..."
 # Restart chain
 config="$GLOBALPATH/config.json"
-NODE_DIR="$( jq -r '.node_data_dir' "$config" )"
-WALLET_DIR="$( jq -r '.wallet_data_dir' "$config" )"
+NODE_DIR="$( /usr/bin/jq -r '.node_data_dir' "$config" )"
+WALLET_DIR="$( /usr/bin/jq -r '.wallet_data_dir' "$config" )"
 #Remove default wallets files in ~/eosio-wallet folder (to clean test)
 /bin/rm -r $WALLET_DIR/*
 
@@ -16,7 +16,7 @@ WALLET_DIR="$( jq -r '.wallet_data_dir' "$config" )"
 
 # Restart chain
 config="$GLOBALPATH/config.json"
-NODE_DIR="$( jq -r '.node_data_dir' "$config" )"
+NODE_DIR="$( /usr/bin/jq -r '.node_data_dir' "$config" )"
 
 $NODE_DIR/start.sh --delete-all-blocks --genesis-json $NODE_DIR/genesis.json
 
@@ -27,12 +27,12 @@ print_test_result() {
     if [[ $T1 -eq 1 ]]; then
 	#echo -e "$T2 - \e[32m[OK]\e[39m" | column -t -s- 
 
-	printf '\e[1;39m%-75s\e[m \e[1;32m%-25s\e[m\n' " $T2" "[OK]"
+	/usr/bin/printf '\e[1;39m%-75s\e[m \e[1;32m%-25s\e[m\n' " $T2" "[OK]"
 	TEST_OK_WALLET=$(($TEST_OK_WALLET+1))
 	TEST_OK=$(($TEST_OK+1))
     else
 	#echo -e "$T2 \t \e[31m[FAILED]\e[39m"
-	printf '\e[1;39m%-75s\e[m \e[1;31m%-25s\e[m\n' " $T2" "[FAILED]"
+	/usr/bin/printf '\e[1;39m%-75s\e[m \e[1;31m%-25s\e[m\n' " $T2" "[FAILED]"
 	TEST_FAILED_WALLET=$(($TEST_FAILED_WALLET+1))
 	TEST_FAILED=$(($TEST_FAILED+1))
     fi
@@ -57,7 +57,7 @@ startCategoryTest(){
     cd $mydir
 
     ENDTIME_GROUP=$(/bin/date +%s.%N)
-    DIFF_GROUP=$(/bin/echo "$ENDTIME_GROUP - $STARTTIME_GROUP" | bc)
+    DIFF_GROUP=$(/bin/echo "$ENDTIME_GROUP - $STARTTIME_GROUP" | /usr/bin/bc)
 
     /bin/echo ""
     /bin/echo -e "\e[1;39m┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫\e[m"
@@ -75,7 +75,7 @@ startCategoryTest(){
 TEST_FAILED=0
 TEST_OK=0
 
-STARTTIME=$(date +%s.%N)
+STARTTIME=$(/bin/date +%s.%N)
 
 #########################################################################################################################
 #########################################################################################################################
@@ -88,7 +88,7 @@ startCategoryTest "tests/02_account"
 
 
 ENDTIME=$(date +%s.%N)
-DIFF=$(echo "$ENDTIME - $STARTTIME" | bc)
+DIFF=$(echo "$ENDTIME - $STARTTIME" | /usr/bin/bc)
 
 /bin/echo ""
 /bin/echo ""
