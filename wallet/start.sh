410a0d1ff768e4cbc8203adf672843d935687a70
Eric <eric@edenx.io>
2018-06-24 16:35:53 +0200

    Oops I did it again

--- wallet/start.sh
+++ wallet/start.sh
@@ -7,4 +7,8 @@ KEOSDBINDIR="../bin/bin/keosd"
 
 /bin/echo "Starting Keosd \n";
 
+if [ ! -d "$DIR" ]; then
+	/bin/mkdir "$DIR"
+fi
+
 $KEOSDBINDIR/keosd --data-dir $DIR --config-dir $DIR > $DIR/stdout.txt 2> $DIR/stderr.txt &  /bin/echo $! > $DIR/keosd.pid
