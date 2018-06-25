74c481a5805192176859c3ba40df0ba81046f9f8
Pete Cheyne <pete.cheyne@gmail.com>
2018-06-25 13:44:36 +0100

    WIP for making this work on a fresh machine.

--- node/start.sh
+++ node/start.sh
@@ -10,7 +10,17 @@
 ##                                                   ##
 #######################################################
 
-NODEOS=~/EOS-Test-Cave/bin/bin/nodeos
-DATADIR=~/test
-$DATADIR/stop.sh
+GLOBALPATH=$(/usr/bin/dirname $(/usr/bin/realpath $0))
+config="$GLOBALPATH/../config.json"
+DATADIR="$( /usr/bin/jq -r '.node_data_dir' "$config" )"
+
+NODEOS="$GLOBALPATH/../bin/bin/nodeos"
+
+/bin/echo "Starting Nodeos";
+
+if [ ! -d "$DATADIR" ]; then
+	/bin/mkdir "$DATADIR"
+fi
+
+$GLOBALPATH/stop.sh
 $NODEOS --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt & /bin/echo $! > $DATADIR/nodeos.pid
