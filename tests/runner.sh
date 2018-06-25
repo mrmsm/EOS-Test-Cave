d0afadd946ad25d78993f3ca8f89bd8e67619375
Pete Cheyne <pete.cheyne@gmail.com>
2018-06-25 15:38:03 +0100

    DRY

---
+++ tests/runner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+######################################################################
+#
+# Runner for all test parent methods
+#
+# Made with <3 by the team @ EOS-BP-Developers
+#
+# Find the code @ https://github.com/EOS-BP-Developers/EOS-Test-Cave
+#
+######################################################################
+
+if [[ ! $GLOBALPATH ]]; then
+    GLOBALPATH="$(dirname $(realpath $0))/../.."
+fi
+
+failed(){
+    echo "0:$TEST_NAME"
+    echo "$TEST_NAME - Failed" >> $GLOBALPATH/log/log_error.log;
+    echo "$1" >> $GLOBALPATH/log/log_error.log;
+    echo "---------------------------------" >> $GLOBALPATH/log/log_error.log;
+}
+
+tpm_stderr="$GLOBALPATH/log/tmp_std_err.log"
