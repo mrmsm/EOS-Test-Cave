9731d787dacef3f3e32c78a70c18bbf39f5274d2
Pete Cheyne <pete.cheyne@gmail.com>
2018-06-25 13:45:03 +0100

    Updates to config.

--- wallet/start.sh
+++ wallet/start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-config="../config.json"
+GLOBALPATH=$(/usr/bin/dirname $(/usr/bin/realpath $0))
+config="$GLOBALPATH/../config.json"
 DIR="$( /usr/bin/jq -r '.wallet_data_dir' "$config" )"
 
-KEOSDBINDIR="../bin/bin/keosd"
+KEOSDBINDIR="$GLOBALPATH/../bin/bin/keosd"
 
-/bin/echo "Starting Keosd \n";
+/bin/echo "Starting Keosd";
 
 if [ ! -d "$DIR" ]; then
 	/bin/mkdir "$DIR"
 fi
 
-$KEOSDBINDIR/keosd --data-dir $DIR --config-dir $DIR > $DIR/stdout.txt 2> $DIR/stderr.txt &  /bin/echo $! > $DIR/keosd.pid
+$KEOSDBINDIR --data-dir $DIR --config-dir $DIR > $DIR/stdout.txt 2> $DIR/stderr.txt &  /bin/echo $! > $DIR/keosd.pid
