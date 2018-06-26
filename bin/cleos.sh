290ad76e74c92dec898a11cc3df34f6601baf8c9
Pete Cheyne <pete.cheyne@gmail.com>
2018-06-26 11:41:40 +0100

    CI test.

--- bin/cleos.sh
+++ bin/cleos.sh
@@ -16,4 +16,4 @@ WALLETHOST="$( /usr/bin/jq -r '.walletAddr' "$config" )"
 NODEHOST="$( /usr/bin/jq -r '.nodeos' "$config" )"
 
 
-$SCRIPTPATH/bin/cleos -u http://$NODEHOST --wallet-url http://$WALLETHOST "$@"
+docker-compose exec keosd /opt/eosio/bin/cleos -u http://nodeosd:8888 --wallet-url http://localhost:8900 "$@"
