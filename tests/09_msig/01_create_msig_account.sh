1842896993ed9098323f578a614516815bd27044
Pete Cheyne <pete.cheyne@gmail.com>
2018-06-28 13:38:01 +0100

    Is it this test?

--- tests/09_msig/01_create_msig_account.sh
+++ tests/09_msig/01_create_msig_account.sh
@@ -1,27 +1,27 @@
-TEST_NAME="Create msig account for multisig test"
+# TEST_NAME="Create msig account for multisig test"
 
-. ../runner.sh
+# . ../runner.sh
 
-accounts=( testmultisig msigconfirm1 msigconfirm2 msigconfirm3 );
+# accounts=( testmultisig msigconfirm1 msigconfirm2 msigconfirm3 );
 
-#----------------------
-PUB_KEY=$( cat $GLOBALPATH/log/wallet_name_testwallet_key.dat | cut -d' ' -f1)
-for NAME in "${accounts[@]}"; do
-  CMD=$( $GLOBALPATH/bin/cleos.sh system newaccount eosio $NAME $PUB_KEY --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "1000.0000 EOS" --transfer 2>$tpm_stderr)
+# #----------------------
+# PUB_KEY=$( cat $GLOBALPATH/log/wallet_name_testwallet_key.dat | cut -d' ' -f1)
+# for NAME in "${accounts[@]}"; do
+#   CMD=$( $GLOBALPATH/bin/cleos.sh system newaccount eosio $NAME $PUB_KEY --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "1000.0000 EOS" --transfer 2>$tpm_stderr)
 
-  ERR=$(cat $tpm_stderr)
-  if [[ $ERR != *"executed transaction"* ]]; then
-      failed "$ERR"
-      rm $tpm_stderr;
-      exit 1
-  fi
+#   ERR=$(cat $tpm_stderr)
+#   if [[ $ERR != *"executed transaction"* ]]; then
+#       failed "$ERR"
+#       rm $tpm_stderr;
+#       exit 1
+#   fi
 
-  CMD=$( $GLOBALPATH/bin/cleos.sh transfer eosio $NAME "1000.0000 EOS" "msig test tokens" -p eosio 2>$tpm_stderr)
-  ERR=$(cat $tpm_stderr)
-  if [[ $ERR != *"executed transaction"* ]]; then
-    failed "$ERR"
-    rm $tpm_stderr;
-    exit 1;
-  fi
-done
+#   CMD=$( $GLOBALPATH/bin/cleos.sh transfer eosio $NAME "1000.0000 EOS" "msig test tokens" -p eosio 2>$tpm_stderr)
+#   ERR=$(cat $tpm_stderr)
+#   if [[ $ERR != *"executed transaction"* ]]; then
+#     failed "$ERR"
+#     rm $tpm_stderr;
+#     exit 1;
+#   fi
+# done
 echo "1:$TEST_NAME"
