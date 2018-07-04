008dc61db5c9473d36ff1103e05bc31b93a30370
mrmsm <mrmsm@naver.com>
2018-07-04 21:46:33 +0900

    failed fix msig contract json create

--- tests/09_msig/10_make_contract_json.sh
+++ tests/09_msig/10_make_contract_json.sh
@@ -6,8 +6,15 @@ TEST_NAME="Generate set contract update json data"
 
 NAME="eosio"
 msig_json="$GLOBALPATH/log/tmp_msig_data.json"
+msig_json_tmp="$GLOBALPATH/log/tmp_msig_data.tmp"
 
-CMD=$(echo -n $($GLOBALPATH/bin/cleos.sh set contract $NAME $GLOBALPATH/contracts_update/eosio.system  -p ${NAME}@active -d -j -s 2> $tpm_stderr) | awk -F"contract\.\.\.\ " '{print $2}' 2> $tpm_stderr  > $msig_json)
+CMD=$(echo -n $($GLOBALPATH/bin/cleos.sh set contract $NAME $GLOBALPATH/contracts_update/eosio.system  -p ${NAME}@active -d -j -s 2> $tpm_stderr) > $msig_json_tmp)
+if [ $(grep "Publishing contract" $msig_json_tmp | wc -l) -ne 0 ]; 
+then
+  CMD=$(cat $msig_json_tmp | awk -F"contract\.\.\.\ " '{print $2}' 2> $tpm_stderr  > $msig_json)
+else
+  CMD=$(cp -a $msig_json_tmp $msig_json 2> $tpm_stderr)
+fi
 
 ERR=$(cat $tpm_stderr)
 if [[ $ERR != "" ]]; then
